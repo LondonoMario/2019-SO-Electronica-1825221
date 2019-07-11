0238aa5960bb6e0564d2c171dc02d9fe02338d5b
Invitado <invitado@s5pc21.eisc.univalle.edu.co>
2019-07-11 12:50:53 -0500

    memory-user

---
+++ 2019_07_11/memory-user.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int arg, char** argv){
+
+	if( arg != 2 ){
+		printf("Por favor digitar un numero\n");
+		exit(-1);
+		}
+	int memorysize = atoi(argv[1]);
+	memorysize = memorysize * 1024 * 1024;
+	int *arreglo = (int*) malloc((sizeof(int) * memorysize));
+	if (arreglo == NULL){
+		printf("Error en la paginacion de memoria\n");
+		exit(-1);
+		}
+	int i = 0;
+//	printf("Memory size %d", arreglo);
+	for(; i < memorysize; i++){
+		arreglo[i] = i;
+
+	}
+	sleep(10);
+	free(arreglo);
+	return 0;
+}
